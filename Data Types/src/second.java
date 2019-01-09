ba1bfe3bd4d89e8e5af2281b510a3bed24e977c4
unknown <anveshak123parashar@gmail.com>
2019-01-09 16:46:09 +0530

    initial commit

---
+++ Data Types/src/second.java
@@ -0,0 +1,8 @@
+class second{
+	public static void main(String args[]){
+		char c='c';
+		int i='c';
+		System.out.println("Integer representation of entered Character is");
+		System.out.println(i);
+	}
+}
